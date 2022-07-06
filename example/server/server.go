cec00351bbe6be002161d83ad109415f0698e0b7
liveej <liveej@qq.com>
2022-07-06 10:06:48 +0800

    Server例子添加心跳、批量位置上报，去除RSA

--- example/server/server.go
+++ example/server/server.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"fmt"
 
 	"gitee.com/coco/go808"
@@ -12,41 +8,6 @@ import (
 	"gitee.com/coco/go808/protocol/extra"
 	log "github.com/sirupsen/logrus"
 )
-
-var privateKey = `-----BEGIN RSA PRIVATE KEY-----
-MIICXgIBAAKBgQDYKDhr0jA+/Fvcf/Fcv7Xn8PC2lkHCUjqE2LdMXAN+4RVve4PC
-hh1dllgECC4doxK5ukEUijJeCwUrzLW2hzoxgN//Z9B6+LuW6twjF4vAVy1M6Abo
-R69KNp2o34tDXm+Vo+4dgcHgBrH9s2ANqY++ImV0se+y6cd4Eo6bqfop/QIDAQAB
-AoGBAMJBuxri5WrlfmS2MqIoxACy3pEojeZl4aNb47bjBl0zSQFMXkgmISPnJihR
-dag60mxJP42G+ObdPoNzUGa+NoN5Gn8ro8esrBrOiUBdFIl7e24xQKd6skxoelIX
-1dc2SzN5vOKFLAbu7r6GCohIpKsi0Icsldgh3REWJhmvkKKhAkEA+JSNOO2kxX5g
-9kbZ8rfhUYN/xWWsCRXBcewocVlEUuit7QlZZeYYhAzoF2UlX7YjbPhWKEJIGPua
-vLiMg+9+iQJBAN6b6tE59CRHd3JeSfs2x4Czudeb0hdOxdR2wnecOexP/LncWrRI
-HOGqqdZFkAsrFyhZ3k2+Eff3fuV7GEg+UtUCQBUtGobB/+pvJLV2PbTmo0Q9bpIT
-Yj934f3hf2SAlUh21/I8fKgonOgK7W6oyDFKI+Rxl21gkCHItVrkYdwPd/kCQQCU
-fWTRU9srKBDhVUv8KrpBe6GH1QT7TyxfYSivKKLqoyBtyjMm9sNtNK49pAFFseSs
-oeXL7fGGeq1G3imAZzJRAkEA7rIZYUjRmmqiNTFILkFo6OZ9cYd7AyZWg2KckUQ4
-pwdWR1prOW0BORYozJc3ZloSN4JZP5Lu2gCerRNGRdenDw==
------END RSA PRIVATE KEY-----
-`
-
-func GetTestPrivateKey() (*rsa.PrivateKey, error) {
-	block, _ := pem.Decode([]byte(privateKey))
-	if block == nil {
-		return nil, errors.New("get private key error")
-	}
-
-	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err == nil {
-		return privateKey, nil
-	}
-
-	privateKey2, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	return privateKey2.(*rsa.PrivateKey), nil
-}
 
 // 处理终端鉴权
 func handleAuthentication(session *go808.Session, message *protocol.Message) {
@@ -92,53 +53,52 @@ func handleReportLocation(session *go808.Session, message *protocol.Message) {
 	session.Reply(message, protocol.T808_0x8100_ResultSuccess)
 }
 
-// 处理上传媒体
-func handleUploadMediaPacket(session *go808.Session, message *protocol.Message) {
-	entity := message.Body.(*protocol.T808_0x0801)
+// 处理批量上报位置
+func handleBatchReportLocation(session *go808.Session, message *protocol.Message) {
+	// 打印消息
+	entity := message.Body.(*protocol.T808_0x0704)
+	fields := log.Fields{
+		"IccID": message.Header.IccID,
+		"类型":    fmt.Sprintf("0x%x", entity.Type),
+	}
 
-	// 读取完整数据包
-	fullPacket := make([]byte, 1024*1024)
-	n, _ := entity.Packet.Read(fullPacket[:])
-	fmt.Println(n)
-
-	session.Send(&protocol.T808_0x8800{
-		MediaID: entity.MediaID,
-	})
-}
-
-// 处理终端 RSA公钥
-func handleUploadRsaPublicKey(session *go808.Session, message *protocol.Message) {
-	// 设置终端公钥
-	entity := message.Body.(*protocol.T808_0x0A00)
-	session.SetPublicKey(entity.PublicKey)
+	for _, rpt := range entity.Items {
+		logRpt := log.Fields{
+			"IccID": message.Header.IccID,
+			"警告":    fmt.Sprintf("0x%x", rpt.Alarm),
+			"状态":    fmt.Sprintf("0x%x", rpt.Status),
+			"纬度":    rpt.Lat,
+			"经度":    rpt.Lng,
+			"海拔":    rpt.Altitude,
+			"速度":    rpt.Speed,
+			"方向":    rpt.Direction,
+			"时间":    rpt.Time,
+		}
+		log.WithFields(logRpt).Info("批量上报位置items")
+	}
+	log.WithFields(fields).Info("批量上报终端位置信息")
 
 	// 回复平台应答
-	session.Reply(message, protocol.T808_0x8100_ResultSuccess)
+	session.Reply(message, 0)
+}
 
-	// 下发平台公钥
-	key := session.GetServer().GetPrivateKey()
-	if key != nil {
-		session.Send(&protocol.T808_0x8A00{
-			PublicKey: &key.PublicKey,
-		})
-	}
+// 处理终端心跳
+func handleHeartBeat(session *go808.Session, message *protocol.Message) {
+	// 回复平台应答
+	session.Reply(message, 0)
 }
 
 func main() {
-	privateKey, err := GetTestPrivateKey()
-	if err != nil {
-		panic(err)
-	}
 
 	server, _ := go808.NewServer(go808.Options{
 		Keepalive:       60,
 		AutoMergePacket: true,
 		CloseHandler:    nil,
-		PrivateKey:      privateKey,
+		//PrivateKey:      privateKey,
 	})
+	server.AddHandler(protocol.MsgT808_0x0002, handleHeartBeat)
 	server.AddHandler(protocol.MsgT808_0x0102, handleAuthentication)
 	server.AddHandler(protocol.MsgT808_0x0200, handleReportLocation)
-	server.AddHandler(protocol.MsgT808_0x0801, handleUploadMediaPacket)
-	server.AddHandler(protocol.MsgT808_0x0A00, handleUploadRsaPublicKey)
+	server.AddHandler(protocol.MsgT808_0x0704, handleBatchReportLocation)
 	server.Run("tcp", 8808)
 }
